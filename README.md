6d62a562ba9d071b059334afcf5d258f9cb0fcc2
Patrick T Coakley <contact@patricktcoakley.com>
2021-05-10 18:22:25 -0500

    Initial version.

---
+++ README.md
@@ -0,0 +1,12 @@
+# swc-starter-template
+
+This is a template project to start using [swc](https://swc.rs/), a "swc is a super-fast typescript / javascript compiler written in rust."
+
+Includes:
+- Pre-configured [Webpack](https://webpack.js.org/) config with [swc-loader](https://swc.rs/docs/usage-swc-loader) and other plugins
+- Support for [Jest](https://jestjs.io/) using [swc-jest](https://swc.rs/docs/usage-swc-jest)
+
+Usage:
+- `npm run dev` for local development
+- `npm run build` for build
+- `npm run test` for tests
---
+++ webpack.common.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.ts',
+    module: {
+        rules: [
+            {
+                test: /\.ts?$/ ,
+                exclude: /node_modules/,
+                loader: "swc-loader"
+            },
+            {
+                test: /\.html$/,
+                loader: "html-loader",
+                options: { minimize: true }
+            },
+            {
+                test: /\.scss/i,
+                use: ["style-loader", "css-loader", "sass-loader"]
+            }
+        ]
+    },
+    resolve: {
+        extensions: [ '.ts', '.js' ],
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            filename: 'index.html',
+            template: path.join(__dirname, 'src/index.html')
+        }),
+        new CleanWebpackPlugin({
+            cleanOnceBeforeBuildPatterns: [
+                'dist'
+            ],
+        }),
+    ],
+};
